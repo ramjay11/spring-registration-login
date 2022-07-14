91f6fe1539d8cdb0d6f8e5096a4b14f557c332f8
Romorosa <ramjay.romorosa@accenture.com>
2022-07-15 00:39:24 +0800

    Initially Commit

---
+++ src/main/java/com/ramjava/registration/SpringSecurityJpa1Application.java
@@ -0,0 +1,13 @@
+package com.ramjava.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramjava/registration/SpringSecurityJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjava.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
